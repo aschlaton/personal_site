148b221f56dfe88ce2a3b76eaa18f82f39e9198b
ethan <ewang22@berkeley.edu>
2025-11-14 14:29:11 -0800

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
   prefetch: {
     prefetchAll: true
   }
